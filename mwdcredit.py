df73d482c0d3833bcf2f9d7feb50a89489bdd391
Patrick Debattista <you@example.com>
2020-09-26 20:55:29 +0400

    Added option to make upto 100 random valid cards, finsihed file checker

--- mwdcredit.py
+++ mwdcredit.py
@@ -13,7 +13,9 @@ def menu():
          menuInput = input()
     if menuInput == "1":
         cardNum = input("Enter your card's number")
-        validate(cardNum)
+        print(validate(cardNum))
+        print(" ")
+        menu()
     elif menuInput == "2":
         fileFound = False
         while fileFound == False:
@@ -25,15 +27,38 @@ def menu():
             else:
                 fileFound = True
         for line in cardFile.readlines():
-            print(line)
             cardNum = line
-            print("["cardNum, validate(cardNum)"]")
+            cardNum = cardNum.strip("\n")
+            print("[", cardNum, validate(cardNum), "]")
+        print(" ")
+        menu()
     elif menuInput == "3":
         cardGenerate()
+        print(" ")
+        menu()
     elif menuInput == "4":
-        return "Thank You!"
+        return print("Thank You!")
 
 def cardGenerate():
+    cardsFile = open("cardsFile.txt", "w+").close()
+    cardsFile = open("cardsFile.txt", "w+")
+    cardsGenerated = []
+    numberOfCards = int(input("How many numbers do you want to genreate?"))
+    while numberOfCards > 100 or numberOfCards < 1:
+        numberOfCards = int(input("How many numbers do you want to genreate?"))
+    for i in range(numberOfCards):
+        cardNum = cardMake()
+        while cardNum in cardsGenerated:
+            cardNum = cardMake()
+        cardsGenerated.append(cardNum)
+    for i in range(numberOfCards):
+        cardsFile.write("%s\n" % cardsGenerated[i])
+    cardsFile.close()
+    cardsFile = open("cardsFile.txt", "r")
+    print(cardsFile.read())
+    
+
+def cardMake():
     cardNum = []
     cnDoubles = []
     while len(cardNum) < 15:
@@ -53,16 +78,16 @@ def cardGenerate():
     for i in range(len(cardNum)):
         cardNum[i] = str(cardNum[i])
     cardNum = "".join(cardNum)
-    return print(cardNum, validate(cardNum))
+    return cardNum
 
 def validate(cardNum):
     cardNum = [cardNum[i:i+1] for i in range(len(cardNum))]
-    if len(cardNum) < 16 or len(cardNum) > 16:
-        return "Card is not valid"
+    if len(cardNum) != 16:
+        return "Card is not valid, Incorrect length"
     for i in range(len(cardNum)):
         if cardNum[i] not in NUMBERS:
-            return "Card is not valid"
-        if i % 2 == 0:
+            return "Card is not valid, Invalid character"
+        if i % 2 == 0 or i == 0:
             cardNum[i] = int(cardNum[i])*2
         else:
             cardNum[i] = int(cardNum[i])
@@ -72,6 +97,6 @@ def validate(cardNum):
     if cardValue % 10 == 0:
         return "Card is valid"
     else:
-        return "Card is not valid"
+        return "Card is not valid, Not divisible by 10"
 
 menu()
